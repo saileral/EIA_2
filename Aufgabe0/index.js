968fb8f38558ba23e9e01db9c4018eee34374863
Alica Sailer <Alica Sailer@LAPTOP-GMSSM62H.fritz.box>
2017-10-15 21:31:05 +0200

    kommentar

--- Aufgabe0/index.js
+++ Aufgabe0/index.js
@@ -1,3 +1,7 @@
+/* Name:Alica Sailer
+     Matrikel:256030
+     Datum:14.10.17
+     Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und nicht diktiert. */
 function dialogFeld() {
     var eingabe = prompt("Sag mir, wie du heißt!", "");
     if (eingabe != null) {
--- Aufgabe0/index.ts
+++ Aufgabe0/index.ts
@@ -1,3 +1,7 @@
+/* Name:Alica Sailer
+     Matrikel:256030
+     Datum:14.10.17
+     Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und nicht diktiert. */
 function dialogFeld(): void {
     let eingabe: string = prompt("Sag mir, wie du heißt!", "");
     if (eingabe != null) {
